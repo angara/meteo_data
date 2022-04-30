3eb664ff0ffb3825592b4ba675dd1790ee147b62
Maxim Penzin <maxp@ya.ru>
2022-04-30 23:18:45 +0800

    init.sql tables

--- sql/init.sql
+++ sql/init.sql
@@ -10,14 +10,14 @@ create extension postgis;
 
 create table meteo_data (
   ts          timestamptz   not null,
-  station     varchar(80)   not null,
+  stid        varchar(80)   not null,
   correct     boolean       not null,   -- record is correct
-  mtype       varchar(20)   not null,   -- [temp,press,humid,wind,gust,wdir, txt]
-  num         decimal(12,5),
-  txt         varchar(80)
+  vtype       varchar(20)   not null,   -- [temp,press,humid,wind,gust,wdir, extra]
+  value       decimal(12,5),
+  extra       jsonb
 );
 
-create index meteo_data_ts_idx on meteo_data (ts, station);
+create index meteo_data_ts_idx on meteo_data (ts, stid);
 
 --
 --
@@ -40,18 +40,21 @@ create index meteo_stations_location_ids on meteo_stations using gist(location);
 create table meteo_last (
   stid       varchar(80) not null,
   ts         timestamptz not null,
-  mtype      varchar(20) not null,
-  num        decimal(12,5),
-  txt        varchar(80)
+  vtype      varchar(20) not null,
+  value      decimal(12,5),
+  extra      jsonb
 );
 
-create unique index meteo_last_idx on meteo_last(stid, ts, mtype);
+create unique index meteo_last_idx on meteo_last(stid, ts, vtype);
 
+
+-- sensor to station mapping, authentcation, handler type, extra params
 --
---
+  create table meteo_sensors (
+    hwid       varchar(80) not null primary key,
+    stid       varchar(80),
+    auth       varchar(200),  -- Authorizaion header value
+    handler    varchar(20),   -- "default|pdm|metar|psw|no_pass"
+    params     jsonb          -- {vtypes:[temp,press,humid,wind,gust,wdir]} -- allowed value types
+  );
 
-create table meteo_sensors (
-  hwid       varchar(80) not null primary key,
-
-
-)
