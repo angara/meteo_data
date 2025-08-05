2f83122f5e0b661f79082d086438c47a86fadbcc
Maxim Penzin <maxp@ya.ru>
2025-08-05 13:36:32 +0800

    example update

--- submit-example.sh
+++ submit-example.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-ENDPOINT=http://rs.angara.net/meteo/_in
+SUBMIT_ENDPOINT=http://rs.angara.net/meteo/_in
+API_ENDPOINT=http://rs.angara.net/meteo/api
+
+# default credentials
 
 USERNAME=_
 PASSWORD=_
 
 HWID=test
+ST=test
 
 T=44
 P=999
 
 QS="hwid=${HWID}&t=${T}&p=${P}"
 
-curl -v -u "${USERNAME}":"${PASSWORD}" "${ENDPOINT}?${QS}" && echo ""
+curl -v -u "${USERNAME}":"${PASSWORD}" "${SUBMIT_ENDPOINT}?${QS}" && echo ""
+
+curl -v -u "${USERNAME}":"${PASSWORD}" "${API_ENDPOINT}/station-info?st=${ST}"
 
 #.
